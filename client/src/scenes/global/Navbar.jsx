5875c5f50ec9fdfcf7940787d6cfb32500080223
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2023-04-15 07:52:49 +0100

    webby

---
+++ client/src/scenes/global/Navbar.jsx
@@ -0,0 +1,94 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton, Typography } from "@mui/material";
+import {
+  PersonOutline,
+  ShoppingBagOutlined,
+  MenuOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme";
+import { setIsCartOpen } from "../../state";
+
+function Navbar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      width="100%"
+      height="60px"
+      backgroundColor="rgb(0, 0, 0, 0.4)"
+      color="black"
+      position="fixed"
+      top="0"
+      left="0"
+      zIndex="1"
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          columnGap="20px"
+          zIndex="2"
+        >
+          <Box color="white">WATCH</Box>
+          <Box color="white">BRACELET</Box>
+          <Box color="white">STRAP</Box>
+        </Box>
+        <Box
+          onClick={() => navigate("/")}
+          sx={{ "&:hover": { cursor: "pointer" } }}
+          color="white"
+        >
+          <Typography variant="h2">WEBBY</Typography>
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          columnGap="20px"
+          zIndex="2"
+        >
+          <IconButton sx={{ color: "white" }}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "white" }}>
+            <PersonOutline />
+          </IconButton>
+          <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            invisible={cart.length === 0}
+            sx={{
+              "& .MuiBadge-badge": {
+                right: 5,
+                top: 5,
+                padding: "0 4px",
+                height: "14px",
+                minWidth: "13px",
+              },
+            }}
+          >
+            <IconButton
+              onClick={() => dispatch(setIsCartOpen({}))}
+              sx={{ color: "white" }}
+            >
+              <ShoppingBagOutlined />
+            </IconButton>
+          </Badge>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Navbar;
---
+++ client/src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,164 @@
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import WatchOutlinedIcon from "@mui/icons-material/WatchOutlined";
+import { shades } from "../../theme";
+
+// imports all images from assets folder
+const importAll = (r) =>
+  r.keys().reduce((acc, item) => {
+    acc[item.replace("./", "")] = r(item);
+    return acc;
+  }, {});
+
+export const heroTextureImports = importAll(
+  require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
+);
+
+const MainCarousel = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  return (
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            right: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateNextIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+    >
+      {Object.values(heroTextureImports).map((texture, index) => (
+        <Box key={`carousel-image-${index}`}>
+          <img
+            src={texture}
+            alt={`carousel-${index}`}
+            style={{
+              width: "100%",
+              height: "700px",
+              objectFit: "cover",
+              backgroundAttachment: "fixed",
+            }}
+          />
+          <Box
+            color="white"
+            borderRadius="1px"
+            textAlign="center"
+            backgroundColor="rgb(0, 0, 0, 0.4)"
+            position="absolute"
+            padding="20px"
+            top="30%"
+            width="50%"
+            alignItems="center"
+            left={isNonMobile ? "25%" : "0"}
+            right={isNonMobile ? undefined : "0"}
+            margin={isNonMobile ? undefined : "0 auto"}
+            minWidth={isNonMobile ? undefined : "100%"}
+          >
+            <Typography color="white">LEARN , BUY, COMMUNITY</Typography>
+            <Typography variant="h1">
+              World's Leading Online Watch Store
+            </Typography>
+          </Box>
+          <Box
+            color="white"
+            borderRadius="1px"
+            textAlign="center"
+            backgroundColor="rgb(0, 0, 0, 0.4)"
+            position="absolute"
+            display="flex"
+            bottom="0px"
+            width="100%"
+            alignItems="center"
+            left={isNonMobile ? "0" : "0"}
+            right={isNonMobile ? undefined : "0"}
+            margin={isNonMobile ? undefined : "0 auto"}
+            minWidth={isNonMobile ? undefined : "100%"}
+          >
+            <Box
+              color="white"
+              padding="20px"
+              borderRadius="1px"
+              textAlign="center"
+              backgroundColor="rgb(0, 0, 0, 0.4)"
+              bottom="0px"
+              borderRight="10px"
+              height="100%"
+              width="100%"
+              left={isNonMobile ? "1" : "0"}
+              right={isNonMobile ? undefined : "0"}
+              margin={isNonMobile ? undefined : "0 auto"}
+            >
+              <Typography variant="h3">BRACELET</Typography>
+            </Box>
+            <Box
+              color="white"
+              padding="20px"
+              borderRadius="1px"
+              textAlign="center"
+              backgroundColor="rgb(0, 0, 0, 0.4)"
+              bottom="0px"
+              borderRight="10px"
+              height="100%"
+              width="100%"
+              left={isNonMobile ? "1" : "0"}
+              right={isNonMobile ? undefined : "0"}
+              margin={isNonMobile ? undefined : "0 auto"}
+            >
+              <Typography color={shades.secondary[200]}>
+                <WatchOutlinedIcon sx={{ fontSize: 40 }} />
+              </Typography>
+              <Typography variant="h3">WATCH</Typography>
+            </Box>
+            <Box
+              color="white"
+              padding="20px"
+              borderRadius="1px"
+              textAlign="center"
+              backgroundColor="rgb(0, 0, 0, 0.4)"
+              bottom="0px"
+              borderRight="10px"
+              height="100%"
+              width="100%"
+              left={isNonMobile ? "1" : "0"}
+              right={isNonMobile ? undefined : "0"}
+              margin={isNonMobile ? undefined : "0 auto"}
+            >
+              <Typography variant="h3">STRAPS</Typography>
+            </Box>
+          </Box>
+        </Box>
+      ))}
+    </Carousel>
+  );
+};
+
+export default MainCarousel;
